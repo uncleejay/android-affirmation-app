f661d9ff414df2b0381a943dfcf3c31b5ab7394c
uncleejay <joseph.c.o216@gmail.com>
2021-09-10 15:35:28 +0100

    started creating layout for recyclerview

---
+++ app/src/main/java/com/example/affirmation/adapter/ItemAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.affirmation.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.R
+import com.example.affirmation.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
+        val textView : TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
